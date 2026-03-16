e88d1079595bf45089b23e1f7429c27b87a50511
SahanNimeshWeerakoon <hsn.weerakoon.m@gmail.com>
2026-03-17 00:38:49 +0530

    Upload image and redirect to the visualizer page with state and display image

--- app/routes/vizualizer.$id.tsx
+++ app/routes/vizualizer.$id.tsx
@@ -1,7 +1,26 @@
+import { useLocation } from 'react-router'
+
 export default function VizualizerId() {
+    const location = useLocation();
+    const { name, initialImage } = location.state || {};
+    if (!location.state) {
+        return (
+            <div className='flex items-center justify-center h-[80vh]'>
+                <h1 className='text-8xl'>No no... not your place..</h1>
+            </div>
+        );
+    }
     return (
-        <div>
-            test
-        </div>
+        <section>
+            <h1>{ name || 'Untitled Project' }</h1>
+            <div className="visualizer">
+                {initialImage && (
+                    <div className="image-container">
+                        <h2>Source Image</h2>
+                        <img src={initialImage} alt='source' />
+                    </div>
+                )}
+            </div>
+        </section>
     )
 }
